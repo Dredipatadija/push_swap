ef2b98f3cc31cdd4a22c07c4306c647bb5b41597
Dredipatadija <andrearmra@gmail.com>
2024-10-15 18:12:58 +0200

    intentando el selection sort

--- sorting.c
+++ sorting.c
@@ -1,19 +1,25 @@
 #include "push_swap.h"
 
-void	ft_sort(t_stack *stack_a, t_stack *stack_b, int *num, int cont)
+void	ft_selection_sort(t_stack *stack_a, t_stack *stack_b, len)
 {
-	int	min;
-	int	pos;
+	t_node	*node;
+	int		index;
+	int		cont;
 
+	cont = len;
+	index = ft_getindex(stack_a);
+	while (cont < (len - 2))
+	{
+		if (index <= (len / 2))
+			ft_rotate(stack_a, index - 1);
+
+	}
+}
+
+void	ft_sort(t_stack *stack_a, t_stack *stack_b, int cont)
+{
 	if (cont == 2)
 		ft_swap(stack_a);
-	min = ft_getmin(stack_a);
-	pos = ft_getpos(stack_a, min);
-	else if (cont == 3)
-	{
-		if (pos == 1)
-			ft_rotate(stack_a);
-		else
-			ft_sort3(stack_a, pos)
-	}
+	else if (cont < 8)
+		ft_selection_sort(stack_a, stack_b);
 }
--- sorting_utils.c
+++ sorting_utils.c
@@ -12,54 +12,21 @@ int	ft_issorted(t_stack *stack)
 	return (1);
 }
 
-int	ft_getmin(t_stack *stack)
+int	ft_getpos(t_stack *stack)
 {
 	t_node	*i;
 	int		min;
-	i = stack->head;
-	min = i->data;
-}
-
-int	ft_getpos(t_stack *stack, int min)
-{
-	t_node	*i;
 	int		pos;
 
 	i = stack->head;
-	pos = 1;
-	while (i->data != min)
+	min = i->data;
+	pos = 0;
+	while (i)
 	{
+		if (i->data < min)
+			min = i->data;
 		i = i->next;
 		pos++;
 	}
 	return (pos);
-}
-
-void	ft_sort3(t_stack *stack, int min, int pos)
-{
-	t_node	*first;
-	t_node	*last;
-	t_node	*mid;
-
-	first = stack->head;
-	while (last->next)
-		last = last->next;
-	mid = first->next;
-	if (pos == 2)
-	{
-		if (first->data > last->data)
-			ft_rotate(stack, 1);
-		else if (first->data < last->data)
-			ft_swap(stack);
-	}
-	else if (pos == 3)
-	{
-		if (first->data > mid->data)
-		{
-			ft_swap(stack);
-			ft_rrotate(stack, 1);
-		}
-		else if (first->data < mid->data)
-			ft_rotate(stack, 2);
-	}
 }
